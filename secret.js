b5d6bb7494023abb1062952987cd887c7110ffd6
Bhavya Garg <bhavya@gatech.edu>
2014-09-20 17:20:27 -0400

    string fix

--- secret.js
+++ secret.js
@@ -1,5 +1,5 @@
 module.exports = {
 
-  db: process.env.MONGODB|| mongodb://<user>:<password>@kahana.mongohq.com:10065/app29808155,
+  db: process.env.MONGODB|| 'mongodb://<user>:<password>@kahana.mongohq.com:10065/app29808155',
 
   };
