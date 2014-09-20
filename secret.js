7f8a354ffb0b7a8ce47343c4b2f5fe84e465c703
keaganlong <klong35@gatech.edu>
2014-09-20 19:05:02 -0400

    Added localhost to secret

--- secret.js
+++ secret.js
@@ -1,5 +1,5 @@
 module.exports = {
 
-  db: process.env.MONGODB|| 'mongodb://<username>:<password>@kahana.mongohq.com:10065/app29808155',
+  db: process.env.MONGODB|| "mongodb://localhost/TeloDev",
   
  };
