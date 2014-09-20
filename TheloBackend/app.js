f01a7c0f05819e322d7f6f9462d173bf5d163f5a
keaganlong <klong35@gatech.edu>
2014-09-20 01:12:21 -0400

    Intial database connection and dummy tests.

--- TheloBackend/app.js
+++ TheloBackend/app.js
@@ -7,14 +7,29 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
+  , path = require('path'),
+  mongoose = require( 'mongoose' );
 
 var app = express();
+mongoose.connect('mongodb://localhost/TheloDev');
 
-// all environments
+var channelSchema = new mongoose.Schema({
+	  name: { type: String }
+});
+
+var Channel = mongoose.model('Channel', channelSchema);
+
+var gtFoodChannel = new Channel({
+	name:"GT-FOOD"
+});
+
+gtFoodChannel.save(function(err, thor) {
+  if (err) return console.error(err);
+  console.dir(thor);
+});
+
 app.set('port', process.env.PORT || 3000);
 
-// development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
@@ -25,5 +40,3 @@ app.get('/users', user.list);
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
-
-console.log(app.get('env'));
